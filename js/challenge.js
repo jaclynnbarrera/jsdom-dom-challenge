d0e27847ae4c188c605e04daf5aa12489a31fecc
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-04-21 17:14:26 -0400

    commit tree 966beb5f54caa16d07c304149a8fd8e9bad51f2e

---
+++ js/challenge.js
@@ -0,0 +1,9 @@
+
+// #increment listener
+document.getElementById("plus").addEventListener("click", function(e){
+    document.getElementById("counter").innerText++
+}) ;
+// decrement listener
+document.getElementById("minus").addEventListener("click", function(e){
+    document.getElementById("counter").innerText--
+}) ;
